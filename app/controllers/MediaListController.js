80e2c96e06d4b22da3f4c6ae3312ac0bfd0ad4d9
DigiZeit <support@digi-zeiterfassung.de>
2014-03-06 15:09:15 +0100

    new build debug

--- app/controllers/MediaListController.js
+++ app/controllers/MediaListController.js
@@ -458,6 +458,7 @@ DigiWebApp.MediaListController = M.Controller.extend({
 					            if (mf.m_id === el.m_id) {
 					            	mf.set("data", fileContent);
 						    		mediaFilesIndex = mediaFilesIndex + 1;
+				        			console.log('mediaFile ' + mf.get('fileName') + ' loaded (' + mediaFilesIndex + ')');
 					            }
 					        });
 						}
