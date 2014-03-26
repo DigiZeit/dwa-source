d0945ebeccb746e84802ba8a6d573a5c0c03658f
DigiZeit <support@digi-zeiterfassung.de>
2014-03-26 17:44:31 +0000

    new build debug

--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 4820'
+              value: 'Build: 4821'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
