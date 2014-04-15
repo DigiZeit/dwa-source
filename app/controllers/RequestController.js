6e8f87dcd29bd071bbf112230078c81bc6be4436
DigiZeit <support@digi-zeiterfassung.de>
2014-04-15 07:32:33 +0000

    new build release

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -23,7 +23,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 4915
+    , softwareVersion: 4916
 
 
     /**
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 4915'
+              value: 'Build: 4916'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
