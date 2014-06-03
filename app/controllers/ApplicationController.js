819f08a54a453e1265ae769b5e0736e6165766b4
DigiZeit <support@digi-zeiterfassung.de>
2014-06-03 08:59:13 +0000

    new build debug

--- app/controllers/ApplicationController.js
+++ app/controllers/ApplicationController.js
@@ -621,30 +621,33 @@ DigiWebApp.ApplicationController = M.Controller.extend({
 	            DigiWebApp.ApplicationController.setImageClass();
 	        });
 		} else {
-            var portraitScreenHeight;
-            var landscapeScreenHeight;
-
-            if(window.orientation === 0 || window.orientation === 180){
-                portraitScreenHeight = $(window).height();
-                landscapeScreenHeight = $(window).width();
-            }
-            else{
-                portraitScreenHeight = $(window).width();
-                landscapeScreenHeight = $(window).height();
-            }
-	        var tolerance = 25;
+//            var portraitScreenHeight;
+//            var landscapeScreenHeight;
+//
+//            if(window.orientation === 0 || window.orientation === 180){
+//                portraitScreenHeight = $(window).height();
+//                landscapeScreenHeight = $(window).width();
+//            }
+//            else{
+//                portraitScreenHeight = $(window).width();
+//                landscapeScreenHeight = $(window).height();
+//            }
+//	        var tolerance = 25;
+//	        $(window).bind('resize', function() {
+//	            if ((window.orientation === 0 || window.orientation === 180) 
+//	            && ((window.innerHeight + tolerance) < portraitScreenHeight)) {
+//	                // keyboard visible in portrait
+//	            	$('[data-role=footer]').hide();
+//	            } else if((window.innerHeight + tolerance) < landscapeScreenHeight) {
+//	                // keyboard visible in landscape
+//	            	$('[data-role=footer]').hide();
+//	            } else {
+//	                // keyboard NOT visible
+//	            	$('[data-role=footer]').show();
+//	            }
+//	            DigiWebApp.ApplicationController.setImageClass();
+//	        });
 	        $(window).bind('resize', function() {
-	            if ((window.orientation === 0 || window.orientation === 180) 
-	            && ((window.innerHeight + tolerance) < portraitScreenHeight)) {
-	                // keyboard visible in portrait
-	            	$('[data-role=footer]').hide();
-	            } else if((window.innerHeight + tolerance) < landscapeScreenHeight) {
-	                // keyboard visible in landscape
-	            	$('[data-role=footer]').hide();
-	            } else {
-	                // keyboard NOT visible
-	            	$('[data-role=footer]').show();
-	            }
 	            DigiWebApp.ApplicationController.setImageClass();
 	        });
 		}
