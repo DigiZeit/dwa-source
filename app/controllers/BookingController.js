f71faa418bc770e264808bbbbdcdc9924cc4ed28
DigiZeit <support@digi-zeiterfassung.de>
2014-08-25 17:58:24 +0200

    new build debug

--- app/controllers/BookingController.js
+++ app/controllers/BookingController.js
@@ -1015,7 +1015,7 @@ DigiWebApp.BookingController = M.Controller.extend({
     , sentBooking: function(obj) {
 
     	if (obj.get("startDateString") == obj.get("endeDateString") && obj.get("startTimeString") == obj.get("endeTimeString")) {
-    		trackError("skip Zeitbuchung start==ende: " + obj.get("startDateString") + ", " + obj.get("startTimeString") + " - " + obj.get("endeDateString") + ", " + obj.get("endeTimeString"));
+    		trackError("skip Zeitbuchung (sent) start==ende: " + obj.get("startDateString") + ", " + obj.get("startTimeString") + " - " + obj.get("endeDateString") + ", " + obj.get("endeTimeString"));
     		return null;
     	};
 
@@ -1106,7 +1106,7 @@ DigiWebApp.BookingController = M.Controller.extend({
     , sentBookingArchived: function(obj) {
     	
     	if (obj.get("startDateString") == obj.get("endeDateString") && obj.get("startTimeString") == obj.get("endeTimeString")) {
-    		trackError("skip Zeitbuchung start==ende: " + obj.get("startDateString") + ", " + obj.get("startTimeString") + " - " + obj.get("endeDateString") + ", " + obj.get("endeTimeString"));
+    		trackError("skip Zeitbuchung (sentArchived) start==ende: " + obj.get("startDateString") + ", " + obj.get("startTimeString") + " - " + obj.get("endeDateString") + ", " + obj.get("endeTimeString"));
     		return null;
     	};
 
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -211,7 +211,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 5550'
+              value: 'Build: 5551'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
