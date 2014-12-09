626408ed26decdb0f4d5b18bb4f36d3665828dc2
DigiZeit <support@digi-zeiterfassung.de>
2014-12-09 15:43:32 +0100

    new build debug

--- app/controllers/ApplicationController.js
+++ app/controllers/ApplicationController.js
@@ -915,11 +915,10 @@ DigiWebApp.ApplicationController = M.Controller.extend({
 			try{$('[id=' + DigiWebApp.SettingsPage.content.closeAppAfterCloseDay.id  + ']').each(function() { $(this).hide(); });}catch(e){}
 			DigiWebApp.SettingsController.setSetting('GPSBackgroundService', NO);
 			DigiWebApp.SettingsController.setSetting('closeAppAfterCloseDay', NO);
-			DigiWebApp.ApplicationController.startNotification();
 		} else {
 			DigiWebApp.ApplicationController.startBgGeo();
-			DigiWebApp.ApplicationController.startNotification();
 		}
+		DigiWebApp.ApplicationController.startNotification();
 
 		if (DigiWebApp.SettingsController.getSetting('debug')) { 
         	DigiWebApp.SettingsController.globalDebugMode = YES; 
