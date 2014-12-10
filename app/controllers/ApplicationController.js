be237511ad9de203d7db50b92197c36fd3528e7d
DigiZeit <support@digi-zeiterfassung.de>
2014-12-10 10:19:31 +0100

    new build release with BuildNo. 5878

--- app/controllers/ApplicationController.js
+++ app/controllers/ApplicationController.js
@@ -778,16 +778,16 @@ DigiWebApp.ApplicationController = M.Controller.extend({
 				});
 				pluginObj.notification.local.promptForPermission();
 		//		window.plugin.notification.local.add({
-		//		    id:         String,  // A unique id of the notifiction
-		//		    date:       Date,    // This expects a date object
-		//		    message:    String,  // The message that is displayed
-		//		    title:      String,  // The title of the message
-		//		    repeat:     String,  // Either 'secondly', 'minutely', 'hourly', 'daily', 'weekly', 'monthly' or 'yearly'
-		//		    badge:      Number,  // Displays number badge to notification
-		//		    sound:      String,  // A sound to be played
-		//		    json:       String,  // Data to be passed through the notification
-		//		    autoCancel: Boolean, // Setting this flag and the notification is automatically canceled when the user clicks it
-		//		    ongoing:    Boolean, // Prevent clearing of notification (Android only)
+		//		      id:         String  // A unique id of the notifiction
+		//		    , date:       Date    // This expects a date object
+		//		    , message:    String  // The message that is displayed
+		//		    , title:      String  // The title of the message
+		//		    , repeat:     String  // Either 'secondly', 'minutely', 'hourly', 'daily', 'weekly', 'monthly' or 'yearly'
+		//		    , badge:      Number  // Displays number badge to notification
+		//		    , sound:      String  // A sound to be played
+		//		    , json:       String  // Data to be passed through the notification
+		//		    , autoCancel: Boolean // Setting this flag and the notification is automatically canceled when the user clicks it
+		//		    , ongoing:    Boolean // Prevent clearing of notification (Android only)
 		//		});
 			} catch(e) {}
 			try {
