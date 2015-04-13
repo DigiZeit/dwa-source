eb0c44b86709e557a78b5bb7c1561e215f07c620
DigiZeit <support@digi-zeiterfassung.de>
2015-04-13 14:16:19 +0200

    new build release with BuildNo. 6088

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -25,7 +25,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 6087
+    , softwareVersion: 6088
 
 
     /**
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -211,7 +211,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 6087'
+              value: 'Build: 6088'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
