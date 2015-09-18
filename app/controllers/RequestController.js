971bf2eef3d997a8718fec667d79fe99805c735d
DigiZeit <support@digi-zeiterfassung.de>
2015-09-18 11:35:21 +0200

    new build debug with BuildNo. 6368

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -25,7 +25,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 6367
+    , softwareVersion: 6368
 
 
     /**
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -211,7 +211,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 6367'
+              value: 'Build: 6368'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
