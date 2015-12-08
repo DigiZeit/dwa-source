fce1caecf2c8ac1429c3881bd31f315af2faac43
DigiZeit <support@digi-zeiterfassung.de>
2015-12-08 17:02:37 +0100

    new build debug with BuildNo. 6740

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 6739
+      softwareVersion: 6740
 
     , getDatabaseServer: function(myFunc, obj) {
     	
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -226,7 +226,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 6739'
+              value: 'Build: 6740'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
