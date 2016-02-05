4ee1da1a78a34926c259faad23b83f711f9fd664
DigiZeit <support@digi-zeiterfassung.de>
2016-02-05 15:40:38 +0100

    new build debug with BuildNo. 6845

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 6844
+      softwareVersion: 6845
 
     , getDatabaseServer: function(myFunc, obj) {
     	
