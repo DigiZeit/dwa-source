97e4d5dd9df96d4390a51fd09c04a33c1dbecbeb
Markus Bäurle <m.baeurle@digi.local>
2016-02-18 14:26:37 +0000

    new build debug with BuildNo. 6894

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 6893
+      softwareVersion: 6894
 
     , getDatabaseServer: function(myFunc, obj) {
     	
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -226,7 +226,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 6893'
+              value: 'Build: 6894'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
