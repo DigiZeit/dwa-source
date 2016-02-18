f078a838cc5524b33e771549c58af804958dcc56
Markus Bäurle <m.baeurle@digi.local>
2016-02-18 14:15:11 +0000

    new build debug with BuildNo. 6893

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 6892
+      softwareVersion: 6893
 
     , getDatabaseServer: function(myFunc, obj) {
     	
