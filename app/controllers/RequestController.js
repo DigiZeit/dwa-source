6cd975e06a3af99d8a063970a8ee6cb6df803c6b
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2016-05-23 12:23:27 +0100

    new build debug with BuildNo. 7030

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 7029
+      softwareVersion: 7030
 
     , getDatabaseServer: function(myFunc, obj) {
     	
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -204,7 +204,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 7029'
+              value: 'Build: 7030'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
