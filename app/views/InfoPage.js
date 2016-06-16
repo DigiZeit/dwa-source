e2d67b03953057b5addcd5d5ef02b7324cbc47bc
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2016-06-16 13:23:36 +0100

    new build debug with BuildNo. 7091

--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 7090'
+              value: 'Build: 7091'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
