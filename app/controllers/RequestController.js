01e38faff528c3839dc5f144e92a42f4873dca8a
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2017-02-01 13:46:37 +0000

    new build release with BuildNo. 7232

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 7231
+      softwareVersion: 7232
 
     , getDatabaseServer: function(myFunc, obj) {
     	
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 7231'
+              value: 'Build: 7232'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
