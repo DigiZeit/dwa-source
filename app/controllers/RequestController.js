6f16acc5b87549d4c5c072bff181146a7a5a9a8a
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2017-02-07 15:43:51 +0000

    new build debug with BuildNo. 7246

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 7245
+      softwareVersion: 7246
 
     , getDatabaseServer: function(myFunc, obj) {
     	
