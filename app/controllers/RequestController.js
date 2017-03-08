88f7cfbe52ccd1516c458f6ee31cdb459692c787
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2017-03-08 10:01:39 +0000

    new build debug with BuildNo. 7251

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 7250
+      softwareVersion: 7251
 
     , getDatabaseServer: function(myFunc, obj) {
     	
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 7250'
+              value: 'Build: 7251'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
