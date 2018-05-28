66bad8edfcf99beea78c1d65b84e1f9d9de79f2f
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2018-05-28 15:42:32 +0100

    new build release with BuildNo. 10922

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -13,7 +13,7 @@ DigiWebApp.RequestController = M.Controller.extend({
 //	, DatabaseServer: null
 //	, DatabaseServerTimestamp: null
     
-      softwareVersion: 10921
+      softwareVersion: 10922
 
     , getDatabaseServer: function(myFunc, obj) {
     	
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 10921'
+              value: 'Build: 10922'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
