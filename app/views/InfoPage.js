1449926cb4eb95bdeef7e2807fb4c1f1c04e411e
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2018-06-13 08:25:03 +0100

    new build release with BuildNo. 10948

--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 10947'
+              value: 'Build: 10948'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
