65e3fafe704af7b9ff03a131b0f67ec885d2216c
Markus Baeurle <m.baeurle@digi-zeiterfassung.de>
2021-02-15 13:15:23 +0000

    new build debug with BuildNo. 11026

--- app/controllers/RequestController.js
+++ app/controllers/RequestController.js
@@ -8,7 +8,7 @@
 // manuell var-checked
 DigiWebApp.RequestController = M.Controller.extend({
 
-      softwareVersion: 11025
+      softwareVersion: 11026
 
     , getDatabaseServer: function(myFunc, obj) {
         return DigiWebApp.JSONDatenuebertragungController.empfangeUrl(function(){
--- app/views/InfoPage.js
+++ app/views/InfoPage.js
@@ -206,7 +206,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 11025'
+              value: 'Build: 11026'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
